e6c4cc841ce07028d1de6845a2f13000fd7e2eac
suziwen <suziwen1@gmail.com>
2025-03-14 01:31:29 +0800

    wait font load

--- docs/templates/frames/jopp/js/index.js
+++ docs/templates/frames/jopp/js/index.js
@@ -2,10 +2,24 @@ const headerEl = document.querySelector(".header");
 const bodyEl = document.querySelector(".body");
 const footerEl = document.querySelector(".footer");
 
+const loadFontFn = async ()=>{
+  var GideonRomanFont = new FontFace("xsjGideonRoman", "url(../../../fonts/GideonRoman-Regular.ttf)", {
+    style: 'normal'
+  });
+  document.fonts.add(GideonRomanFont)
+
+  var OswaldFont = new FontFace("xsjOswald", "url(../../../fonts/Oswald-Bold.ttf)", {
+    style: 'normal'
+  });
+  document.fonts.add(OswaldFont)
+  await GideonRomanFont.load();
+  await OswaldFont.load();
+}
 
 window.joppInit = async function (opts) {
   const { templateInfo, documentInfo, pdfInfo } = opts;
   document.body.style.display = "block"; // display flex will cause layout bug when print pdf.
+  await loadFontFn();
   const saveDate = new Date();
   switch (templateInfo.region) {
     case "header": {
